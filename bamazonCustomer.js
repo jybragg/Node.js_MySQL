e4a78adec3b7a2fcdbe1ad4a38b0ab76ffcbac3d
Jasmin <jasymatos@gmail.com>
2019-08-17 21:14:06 -0400

    added function to update database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,5 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var fs = require("fs");
-
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,127 +13,59 @@ var connection = mysql.createConnection({
     //password
     password: "root",
     database: "bamazon"
-})
+});
 
 connection.connect(function (err) {
     if (err) throw err;
-    //console.log("connected as id " + connection.threadId);
-    afterConnection();
-    search();
-    //connection.end();
-})
+    //to test connection
+    //console.log("connected as id " + connection.threadId) 
+    inventory(search);
+});
 
-//queries here
-function afterConnection() {
+var inventory = function (invTable) {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        //first display all of the items available for sale
-        //console.log(res);
-        //console.table(res);
-        //search();
-        //
+        console.table(res);
+        invTable();
     })
 };
-// var idInput;
-// var quantityInput;
-// var inventory;
-// var updatedQuantity;
-// var itemId;
-// var howMany;
-// The app should then prompt users with two messages.
-function search() {
-    inquirer.prompt({
-        name: "itemId",
-        type: "input",
-        message: "What is the ID of the product you would like to buy?",
-    },
-        //The second message asks how many units of the product they would like to buy
+
+var search = function () {
+    inquirer.prompt([
         {
-            name: "howMany",
+            name: "choice",
             type: "input",
-            message: "How many units would you like?",
-        })
-        .then(function(answer) {
+            message: "What is the item you woukd like to buy? Enter item id:"
+        },
+        {
+            name: "ChosenAmount",
+            type: "input",
+            message: "How many would you like to buy?"
+        }
+    ]).then(function (answer) {
 
-            var quantity = answer.quantity;
-            var itemID = answer.id;
+        var userChoice = answer.choice;
+        var quantity = parseInt(answer.ChosenAmount);
 
-            connection.query('SELECT * FROM products WHERE item_id =' + itemID, function (err, selectedItem) {
-                if (err) throw err;
+        connection.query("SELECT stock_quantity FROM products WHERE item_id = ?", [userChoice], function (err, data) {
+            if (err) throw err;
 
-               if (selectedItem[0].qty - quantity) {
+            var amtNum = data[0].stock_quantity;
+            var newAmt = amtNum - quantity;
 
-                    console.log("iIn Stock: " + selectedItem[0].qty + " Order Quantity: " + quantity);
-
-                    // .then(function (answer) {
-                    //     console.log(answer);
-                    //     input = answer.item_id;
-                    //     // quantityInput = answer.stock_quantity;
-                    //     console.log(input);
-                    //     // console.log(quantityInpu);
-                    //checkInventory();
-                }
-            })
-       })
-    }
-    // function checkInventory() {
-    //     connection.query("SELECT stock_quantity FROM products WHERE item_id = ? ;", [input], function (err, res) {
-    //         if (err) throw err;
-    //         console.log(res);
-    //         inventory = res.stock_quantity;
-    //         checkAvailability();
-    //     })
-
-    //     function checkAvailability() {
-    //         if (quantityInput > inventory) {
-    //             console.log("Insufficient quantity!")
-    //         }
-    //         else if ((quantityInput < inventory) || (quantityInput === inventory)) {
-    //             updateInventory();
-    //         }
-    //     }
-
-    // }
-
-    // function updateInventory() {
-    //     //console.log(inventory)
-    //     //console.log(quantityInput)
-    //     updatedQuantity = inventory - quantityInput
-    //     console.log(updatedQuantity)
-    //     connection.query("UPDATE products SET ? WHERE ?", function (err, res) {
-    //         if (err) throw err;
-    //         console.log("You have succesfully purchased this item!")
-    //         console.table(res);
-    //         showUpdatedTable()
-    //     })
-    // }
-
-    // function showUpdatedTable() {
-    //     connection.query("SELECT * FROM products;", function (err, res) {
-    //         if (err) throw err;
-    //         console.table(res);
-    //         connection.end();
-    //     })
-    // }
-
-
-
-// var query = "SELECT item_id FROM products WHERE ?";
-// connection.query(query, { item: answer.item_id }, function (err, res) {
-//     if (err) throw err;
-//     for (var i = 0; i < res.length; i++) {
-//         console.log("Position: " + res[i].position + " || Product: " + res[i].product_name);
-//     }
-// })
-//--------------------------------------------------------------------------------------------------------------------//
-// Once the customer has placed the order, your application should CHECK if your store has enough of the product to meet
-//the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
-
-//--------------------------------------------------------------------------------------------------------------------//
-
+            if (newAmt >= 0) {
+                connection.query("UPDATE products SET ? WHERE ?",
+                    [{ stock_quantity: newAmt },
+                    { item_id: userChoice }],
+                    function (err, res) {
+                        if (err) throw err;
+                        console.log("Your order has been placed for: " + quantity + " \nNow there are only " + newAmt + " available")
+                    });
+            }
+            else {
+                console.log("Insufficient Quantity!");
+            };
+            connection.end();
+        });
+    });
+};
